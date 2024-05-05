d27faabeddac9010b6dff452d6f51a4baf5aefc9
Alex Lobascio <alex@Lobasc.io>
2024-05-05 14:15:24 -0700

    set up ruby version, gemfile, and rspec with sanity test

---
+++ Gemfile
@@ -0,0 +1,5 @@
+ruby '3.2.2'
+
+source "https://rubygems.org"
+
+gem 'rspec', '3.13.0'
---
+++ spec/base_spec.rb
@@ -0,0 +1,6 @@
+describe 'sanity test' do
+  it 'should have working expectations' do
+    expect(true).to be true
+    expect(true).not_to be false
+  end
+end
